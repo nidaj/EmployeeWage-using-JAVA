700768f8094e2bd5c2ae0512de93ff3db5f24f5e
nida.j <nida.jawre@gmail.com>
2020-11-09 21:27:03 +0530

    Added: Part time employee wage calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,18 @@ class EmployeeWage{
   public static void main(String[] args) {
 	int full_wage=160;
     	int no_wage=0; 
+	int part_time=80;
 	Random rand = new Random();
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	int attendance = rand.nextInt(2);
+	int attendance = rand.nextInt(3);
     	if(attendance==1){
       		System.out.println("Wage= "+full_wage);
     	}
-    	else{
-      		System.out.println("wage= "+no_wage);
+    	else if(attendance==2){
+      		System.out.println("wage= "+part_time);
     	}
+	else{
+		 System.out.println("wage= "+no_wage);
+	}
   }
 }
