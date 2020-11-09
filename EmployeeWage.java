79b64f549179598dc2e0b51bc557a1449721566f
nida.j <nida.jawre@gmail.com>
2020-11-09 21:36:22 +0530

    Modified: Implemented using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,15 +2,20 @@ import java.util.Random;
 class EmployeeWage{
   public static void main(String[] args) {
 	int full_wage=160;
+	int part_time=80;
     	int no_wage=0; 
 	Random rand = new Random();
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	int attendance = rand.nextInt(2);
-    	if(attendance==1){
+	int attendance = rand.nextInt(3);
+    	switch(attendance){
+	case 1: 
       		System.out.println("Wage= "+full_wage);
-    	}
-    	else{
-      		System.out.println("wage= "+no_wage);
+    		break;
+    	case 2 :
+      		System.out.println("Wage= "+part_time);
+		break;
+	case 0 :
+		System.out.println("Wage= "+no_wage);
     	}
   }
 }
