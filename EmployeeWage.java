493b19ae9d8d6aa2ce42330286a45c1334d047d6
nida.j <nida.jawre@gmail.com>
2020-11-09 21:23:05 +0530

    Added: code to calculate full wage and no wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 class EmployeeWage{
   public static void main(String[] args) {
+	int full_wage=160;
+    	int no_wage=0; 
 	Random rand = new Random();
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 	int attendance = rand.nextInt(2);
     	if(attendance==1){
-      		System.out.println("Present");
+      		System.out.println("Wage= "+full_wage);
     	}
     	else{
-      		System.out.println("Absent");
+      		System.out.println("wage= "+no_wage);
     	}
   }
 }
