618787d25a62c85ac3f52a0d69c1f65da97d805f
nida.j <nida.jawre@gmail.com>
2020-11-09 21:12:58 +0530

    Added: Code to check employee is present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,14 @@
+import java.util.Random;
 class EmployeeWage{
   public static void main(String[] args) {
-    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	Random rand = new Random();
+	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	int attendance = rand.nextInt(2);
+    	if(attendance==1){
+      		System.out.println("Present");
+    	}
+    	else{
+      		System.out.println("Absent");
+    	}
   }
 }
