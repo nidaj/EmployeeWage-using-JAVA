6306fb89d4b452c236893657bcf55ba314ddd8ca
nida.j <nida.jawre@gmail.com>
2020-11-10 21:44:13 +0530

    Modified: Refactored the code

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,37 @@
 import java.util.Random;
 class EmployeeWage{
+  static final int full_wage=160;
+  static final int no_wage=0;
+  static final int part_time=80;
+  public void calculate_wage(){
+    int sum=0;
+    int attendance;
+    Random rand = new Random();
+    System.out.println("Day\tWage\tSum");
+    for(int d=1;d<=20;d++){
+      attendance = rand.nextInt(3);
+      switch(attendance){
+      case 1:
+        sum=sum+full_wage;
+        System.out.println(d+"\t"+full_wage+"\t"+sum);
+        break;
+      case 2 :
+        sum=sum+part_time;
+        System.out.println(d+"\t"+part_time+"\t"+sum);
+        break;
+      case 0 :
+        sum=sum+no_wage;
+        System.out.println(d+"\t"+no_wage+"\t"+sum);
+      }
+    }
+  }
+  
   public static void main(String[] args) {
-        int full_wage=160;
-        int part_time=80;
-        int no_wage=0;
-        Random rand = new Random();
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int attendance;
-        System.out.println("Days\tWage");
-        for(int d=1;d<=20;d++){
-          attendance = rand.nextInt(3);
-          switch(attendance){
-          case 1:
-                System.out.println(d+"\t\t"+full_wage);
-                break;
-          case 2 :
-                System.out.println(d+"\t\t"+part_time);
-                break;
-          case 0 :
-                System.out.println(d+"\t\t"+no_wage);
-          }
-
-        }
+    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    EmployeeWage obj = new EmployeeWage();
+    obj.calculate_wage();
   }
 }
+
+
+
