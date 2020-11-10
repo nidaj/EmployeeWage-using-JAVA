b9f2ae27f462ec9ab95fc961028eb3602155dbbf
nida.j <nida.jawre@gmail.com>
2020-11-10 21:19:48 +0530

    Added: additional changes done for total wages calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,21 +4,25 @@ class EmployeeWage{
         int full_wage=160;
         int part_time=80;
         int no_wage=0;
+	int sum=0;
         Random rand = new Random();
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int attendance;
-        System.out.println("Days\tWage");
+        System.out.println("Days\tWage\tsum");
         for(int d=1;d<=20;d++){
           attendance = rand.nextInt(3);
           switch(attendance){
           case 1:
-                System.out.println(d+"\t\t"+full_wage);
+		sum=sum+full_wage;
+                System.out.println(d+"\t"+full_wage+"\t"+sum);
                 break;
           case 2 :
-                System.out.println(d+"\t\t"+part_time);
+		sum=sum+part_time;
+                System.out.println(d+"\t"+part_time+"\t"+sum);
                 break;
           case 0 :
-                System.out.println(d+"\t\t"+no_wage);
+		sum=sum+no_wage;
+                System.out.println(d+"\t"+no_wage+"\t"+sum);
           }
 
         }
